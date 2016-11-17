c865c0879a3c455b47ed25f78ba373b6200df9a0
MONKCs studios <ape@monkcs.se>
2016-11-17 23:25:46 +0100

    Clone relay-system arduino sketch to onboard-system

---
+++ onboard-system/arduino/onboard/onboard.ino
@@ -0,0 +1,44 @@
+#include <BufferedSerial.h>
+#include <ByteBuffer.h>
+#include <SoftwareSerial.h>
+
+BufferedSerial serial = BufferedSerial(256, 256);
+ByteBuffer transmitBuffer;
+
+SoftwareSerial serialToRaspberry(10, 11);                        // RX, TX pinout on Arduino Uno
+
+void setup() {
+  serial.init(0, 9600);                                          // Initialize the serial communication
+  serial.setPacketHandler(MessageReceiver);                      // MessageReceiver will be called when a new "packet" arrive
+  serialToRaspberry.begin(9600);                                 // Set the data rate for the SoftwareSerial port
+  transmitBuffer.init(30);                                       // Initialize the send buffer that we will use to send data
+}
+
+void loop() {
+  serial.update();
+
+}
+
+void MessageReceiver(ByteBuffer* packet) {
+  int bufferLenght = packet->getSize();                          // Get the size of the message
+  char message[256] = {0};                                       // Set array to null
+  for (int counter = 0; counter < bufferLenght; counter++) {    
+    message[counter] = packet->get();                            // Copying all char from packet into message[] array
+  }
+
+  if (!strcmp(message, "MESSAGE-STRING")) {
+    // Do something, like sending data back to raspberry pi
+  }
+}
+
+bool MessageTransmitter(char *message[], int length) {
+  if (!serial.isBusySending()) {
+    transmitBuffer.clear();
+    for (int counter = 0; counter <= length; counter++) {
+      // Put one char at the time into transmitBuffer
+      transmitBuffer.put(message[counter]);
+    }
+    serial.sendSerialPacket( &transmitBuffer );
+  }
+  return true;
+}

6ccad80b0fa79b44779393671ff544e21e29200b
MONKCs studios <ape@monkcs.se>
2016-11-17 23:13:25 +0100

    Add SoftwareSerial to talk to raspberry pi

--- relay-system/arduino/relay/relay.ino
+++ relay-system/arduino/relay/relay.ino
@@ -1,17 +1,17 @@
 #include <BufferedSerial.h>
 #include <ByteBuffer.h>
+#include <SoftwareSerial.h>
 
 BufferedSerial serial = BufferedSerial(256, 256);
 ByteBuffer transmitBuffer;
 
-void setup() {
-  // Initialize the serial communication
-  serial.init(0, 9600);
-  serial.setPacketHandler(MessageReceiver);
-  
-  // Initialize the send buffer that we will use to send data
-  transmitBuffer.init(30);
+SoftwareSerial serialToRaspberry(10, 11);                        // RX, TX pinout on Arduino Uno
 
+void setup() {
+  serial.init(0, 9600);                                          // Initialize the serial communication
+  serial.setPacketHandler(MessageReceiver);
+  serialToRaspberry.begin(4800);                                 // Set the data rate for the SoftwareSerial port
+  transmitBuffer.init(30);                                       // Initialize the send buffer that we will use to send data
 }
 
 void loop() {
@@ -23,7 +23,7 @@ void MessageReceiver(ByteBuffer* packet) {
   int bufferLenght = packet->getSize();                          // Get the size of the message
   char message[256] = {0};                                       // Set array to null
   for (int counter = 0; counter < bufferLenght; counter++) {    
-    message[counter] = packet->get();                             // Copying all char from packet into message[] array
+    message[counter] = packet->get();                            // Copying all char from packet into message[] array
   }
 
   if (!strcmp(message, "MESSAGE-STRING")) {
